46b70bdd3d2efd61aa889cf17c7aad2aec74f1ff
Nina Kristel Cheng <nkmcheng@live.com>
2017-02-21 12:37:46 +0800

    added styles, changed login styles

---
+++ resources/views/layouts/login.blade.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="{{ config('app.locale') }}">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'AIMS') }}</title>
+
+    <!-- Styles -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/styles.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/login.css') }}" rel="stylesheet">
+
+    <!-- Scripts -->
+    <script>
+        window.Laravel = {!! json_encode([
+            'csrfToken' => csrf_token(),
+        ]) !!};
+    </script>
+</head>
+<body>
+<div id="app">
+    <div class="login-outer-wrapper">
+        <div class="login-inner-wrapper col-md-6 col-xs-12 col-sm-12 col-md-offset-3">
+            <div class="login-box">
+                <div class="logo" style="textAlign: center">
+                    <img src="/images/aims-logo.png" alt="aims logo" style="height: 250px"/>
+                </div>
+
+                <div class="login-form">
+                    @yield('content')
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<!-- Scripts -->
+<script src="{{ asset('js/app.js') }}"></script>
+</body>
+</html>
