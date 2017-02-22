b97d7edacac30c19fbb81e2aeb9b29b781fb9e9a
Alleo Indong <freedom_war550@live.com.ph>
2017-02-22 10:31:24 +0800

    fix bug

--- resources/views/ae/jolist/details/mom/others.blade.php
+++ resources/views/ae/jolist/details/mom/others.blade.php
@@ -1,6 +1,5 @@
 <div class="tab-pane" id="other-details">
-    <textarea class="attendees" placeholder="Place some text here"
+    <textarea class="attendees" name="other_details" placeholder="Place some text here"
               style="width: 100%; height: 200px; font-size: 14px;
-              line-height: 18px; border: 1px solid #dddddd; padding: 10px" >
-    </textarea>
+              line-height: 18px; border: 1px solid #dddddd; padding: 10px" >{{ $mom->other_details }}</textarea>
 </div>
--- resources/views/ae/jolist/details/mom/step.blade.php
+++ resources/views/ae/jolist/details/mom/step.blade.php
@@ -1,6 +1,5 @@
 <div class="tab-pane" id="next-step">
-    <textarea class="attendees" placeholder="Place some text here"
+    <textarea class="attendees" name="next_step_deliverables" placeholder="Place some text here"
               style="width: 100%; height: 200px; font-size: 14px;
-              line-height: 18px; border: 1px solid #dddddd; padding: 10px" >
-    </textarea>
+              line-height: 18px; border: 1px solid #dddddd; padding: 10px" >{{ $mom->next_step_deliverables }}</textarea>
 </div>
