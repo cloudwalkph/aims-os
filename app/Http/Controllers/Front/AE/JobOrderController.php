d2ff2cc4570d8f30ee0afec0c013ce54c45c0938
Alleo Indong <freedom_war550@live.com.ph>
2017-02-22 11:19:51 +0800

    animation details

--- app/Http/Controllers/Front/AE/JobOrderController.php
+++ app/Http/Controllers/Front/AE/JobOrderController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Front\AE;
 
 use App\Http\Controllers\Controller;
 use App\Models\JobOrder;
+use App\Models\JobOrderAnimationDetail;
 use App\Models\JobOrderDetail;
 use App\Models\JobOrderMom;
 use Illuminate\Http\Request;
@@ -51,6 +52,9 @@ class JobOrderController extends Controller
         $detail = JobOrderDetail::where('job_order_id', $jo->id)
             ->orderBy('id', 'desc')->first();
 
+        $animations = JobOrderAnimationDetail::where('job_order_id', $jo->id)
+            ->get();
+
         $brands = [];
         foreach ($jo->clients as $client) {
             array_push($brands, ucwords($client->brands[0]->name));
@@ -60,7 +64,8 @@ class JobOrderController extends Controller
             ->with('jo', $jo)
             ->with('brands', $brands)
             ->with('mom', $mom)
-            ->with('detail', $detail);
+            ->with('detail', $detail)
+            ->with('animations', $animations);
     }
 
     public function saveJobOrderMOM(Request $request, $joId)
@@ -77,6 +82,21 @@ class JobOrderController extends Controller
         $input = $request->all();
         $input['job_order_id'] = $joId;
         $detail = JobOrderDetail::create($input);
+
+        return redirect()->back();
+    }
+
+    public function saveAnimationDetails(Request $request, $joId)
+    {
+        $detail = null;
+        \DB::transaction(function() use ($request, &$detail, $joId) {
+            // Get all the input
+            $input = $request->all();
+            $input['job_order_id'] = $joId;
+
+            // Create new jo animation details
+            $detail = JobOrderAnimationDetail::create($input);
+        });
 
         return redirect()->back();
     }
--- resources/views/ae/jolist/details/event/modals/animation.blade.php
+++ resources/views/ae/jolist/details/event/modals/animation.blade.php
@@ -5,18 +5,18 @@
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
                 <h4 class="modal-title" id="myModalLabel">Add Animation Details</h4>
             </div>
-            <div class="modal-body">
-                <form>
+            <form method="POST" action="/ae/jo/{{ $jo->id }}/animation" id="animationForm">
+                <div class="modal-body">
                     <div class="row">
-
+                        {{ csrf_field() }}
                         <div class="col-md-12 form-group text-input-container">
                             <label class="control-label" for="particular">Particulars</label>
-                            <input type="text" name="particular" placeholder="Particulars" class="form-control" />
+                            <input type="text" name="particular" required placeholder="Particulars" class="form-control" />
                         </div>
 
                         <div class="col-md-12 form-group text-input-container">
                             <label class="control-label" for="target_activity">Target Activity</label>
-                            <input type="text" name="target_activity" placeholder="Target Activity" class="form-control" />
+                            <input type="text" name="target_activity" required placeholder="Target Activity" class="form-control" />
                         </div>
 
                         <div class="col-md-12 text-center">
@@ -25,27 +25,27 @@
 
                         <div class="col-md-4 form-group text-input-container">
                             <label class="control-label" for="target_selling">Selling</label>
-                            <input type="text" name="target_selling" placeholder="Selling" class="form-control" />
+                            <input type="number" name="target_selling" required placeholder="Selling" class="form-control" />
                         </div>
 
                         <div class="col-md-4 form-group text-input-container">
                             <label class="control-label" for="target_flyering">Flyering</label>
-                            <input type="text" name="target_flyering" placeholder="Flyering" class="form-control" />
+                            <input type="number" name="target_flyering" required placeholder="Flyering" class="form-control" />
                         </div>
 
                         <div class="col-md-4 form-group text-input-container">
                             <label class="control-label" for="target_survey">Survey</label>
-                            <input type="text" name="target_survey" placeholder="Survey" class="form-control" />
+                            <input type="number" name="target_survey" required placeholder="Survey" class="form-control" />
                         </div>
 
                         <div class="col-md-4 form-group text-input-container">
                             <label class="control-label" for="target_experiment">Experiment</label>
-                            <input type="text" name="target_experiment" placeholder="Experiment" class="form-control" />
+                            <input type="number" name="target_experiment" required placeholder="Experiment" class="form-control" />
                         </div>
 
                         <div class="col-md-4 form-group text-input-container">
                             <label class="control-label" for="target_others">Others</label>
-                            <input type="text" name="target_others" placeholder="Others" class="form-control" />
+                            <input type="number" name="target_others" required placeholder="Others" class="form-control" />
                         </div>
 
                         <div class="col-md-12">
@@ -54,21 +54,20 @@
 
                         <div class="col-md-12 form-group text-input-container">
                             <label class="control-label" for="target_duration">Target Duration</label>
-                            <input type="text" name="target_duration" placeholder="Target Duration" class="form-control" />
+                            <input type="number" name="target_duration" required placeholder="Target Duration" class="form-control" />
                         </div>
 
                         <div class="col-md-12 form-group text-input-container">
                             <label class="control-label" for="target_areas">Areas</label>
-                            <input type="text" name="target_areas" placeholder="Areas" class="form-control" />
+                            <input type="number" name="target_areas" required placeholder="Areas" class="form-control" />
                         </div>
                     </div>
-                </form>
-
-            </div>
-            <div class="modal-footer">
-                <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-                <button type="button" class="btn btn-primary">Add</button>
-            </div>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                    <button type="submit" class="btn btn-primary" id="saveAnimationDetail">Add</button>
+                </div>
+            </form>
         </div>
     </div>
 </div>
