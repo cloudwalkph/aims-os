a738af9cf8c843168b1de6a77269483d090c68fe
Roel Rosil <roelrosil1705@gmail.com>
2017-02-28 18:13:09 +0800

    Migration and model

---
+++ database/migrations/2017_02_28_024144_create_accountings_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAccountingsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('accountings', function (Blueprint $table) {
+            $table->increments('id');
+            $table->text('job_order_no');
+            $table->text('ceNumber')->nullable();
+            $table->text('ceFile')->nullable();
+            $table->date('ceDateUpdated')->nullable();
+            $table->date('transmittalDate')->nullable();
+            $table->date('transmittalDateUpdated')->default();
+            $table->text('invoiceNumber')->nullable();
+            $table->text('invoiceFile')->nullable();
+            $table->date('invoiceDateUpdated')->nullable();
+            $table->date('paidDate')->nullable();
+            $table->text('paidFile')->nullable();
+            $table->date('paidDateUpdated')->nullable();
+            $table->text('remarks')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('accountings');
+    }
+}
