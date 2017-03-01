1fa4ef7485c51d9b161c8a377437aac1a191dadc
Nina Kristel Cheng <nkmcheng@live.com>
2017-03-01 10:38:43 +0800

    added vue form for adding of departments involved

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -53,6 +53,10 @@ Vue.component('vehicle-request-table', require('./components/job-orders/requests
 
 Vue.component('vehicle-request-form', require('./components/job-orders/requests/vehicle/commons/form.vue'));
 
+Vue.component('department-involvement-table', require('./components/job-orders/involvement/involvement-table.vue'));
+
+Vue.component('department-involvement-form', require('./components/job-orders/involvement/commons/form.vue'));
+
 require('./components/inventory');
 
 /* HR */
---
+++ resources/assets/js/components/job-orders/involvement/involvement-table.vue
@@ -0,0 +1,196 @@
+<template>
+    <div>
+        <department-involved-filter-bar></department-involved-filter-bar>
+        <vuetable ref="vuetable"
+                  api-url="/api/v1/job-order-department-involvements"
+                  :fields="fields"
+                  pagination-path=""
+                  :css="css.table"
+                  :sort-order="sortOrder"
+                  :multi-sort="true"
+                  detail-row-component="my-detail-row"
+                  :append-params="moreParams"
+                  @vuetable:pagination-data="onPaginationData"
+        ></vuetable>
+        <div class="vuetable-pagination">
+            <vuetable-pagination-info ref="paginationInfo"
+                                      info-class="pagination-info"
+            ></vuetable-pagination-info>
+            <vuetable-pagination ref="pagination"
+                                 :css="css.pagination"
+                                 :icons="css.icons"
+                                 @vuetable-pagination:change-page="onChangePage"
+            ></vuetable-pagination>
+        </div>
+
+    </div>
+</template>
+
+
+<script>
+    import accounting from 'accounting'
+    import moment from 'moment'
+    import Vuetable from 'vuetable-2/src/components/Vuetable'
+    import VuetablePagination from 'vuetable-2/src/components/VuetablePagination'
+    import VuetablePaginationInfo from 'vuetable-2/src/components/VuetablePaginationInfo'
+    import Vue from 'vue'
+    import VueEvents from 'vue-events'
+    import DepartmentInvolvedCustomActions from './commons/CustomActions'
+    import DepartmentInvolvedFilterBar from './commons/FilterBar'
+
+    Vue.use(VueEvents)
+    Vue.component('department-involved-actions', DepartmentInvolvedCustomActions)
+    Vue.component('department-involved-filter-bar', DepartmentInvolvedFilterBar)
+
+    export default {
+        components: {
+            Vuetable,
+            VuetablePagination,
+            VuetablePaginationInfo,
+        },
+        data() {
+            return {
+                fields: [
+                    {
+                        name: '__sequence',
+                        title: '#',
+                        titleClass: 'text-right',
+                        dataClass: 'text-right'
+                    },
+                    {
+                        name: '__checkbox',
+                        titleClass: 'text-center',
+                        dataClass: 'text-center',
+                    },
+                    {
+                        name: 'name',
+                        sortField: 'name',
+                        title: 'Department'
+                    },
+                    {
+                        name: 'deliverables',
+                        sortField: 'deliverables',
+                        title: 'Deliverables'
+                    },
+                    {
+                        name: 'deadline',
+                        sortField: 'deadline',
+                        titleClass: 'text-center',
+                        dataClass: 'text-center',
+                        callback: 'formatDate|DD-MM-YYYY',
+                        title: 'Deadline'
+                    },
+                    {
+                        name: 'created_at',
+                        sortField: 'created_at',
+                        titleClass: 'text-center',
+                        dataClass: 'text-center',
+                        callback: 'formatDate|DD-MM-YYYY',
+                        title: 'Created Date'
+                    },
+                    {
+                        name: 'status',
+                        sortField: 'status',
+                        title: 'Status'
+                    },
+                    {
+                        name: '__component:department-involved-actions',
+                        title: 'Actions',
+                        titleClass: 'text-center',
+                        dataClass: 'text-center'
+                    }
+                ],
+                css: {
+                    table: {
+                        tableClass: 'table table-bordered table-striped table-hover',
+                        ascendingIcon: 'glyphicon glyphicon-chevron-up',
+                        descendingIcon: 'glyphicon glyphicon-chevron-down'
+                    },
+                    pagination: {
+                        wrapperClass: 'pagination',
+                        activeClass: 'active',
+                        disabledClass: 'disabled',
+                        pageClass: 'page',
+                        linkClass: 'link',
+                    },
+                    icons: {
+                        first: 'glyphicon glyphicon-step-backward',
+                        prev: 'glyphicon glyphicon-chevron-left',
+                        next: 'glyphicon glyphicon-chevron-right',
+                        last: 'glyphicon glyphicon-step-forward',
+                    },
+                },
+                sortOrder: [
+                    { field: 'created_at', sortField: 'created_at', direction: 'asc'}
+                ],
+                moreParams: {}
+            }
+        },
+        methods: {
+            formatDate (value, fmt = 'D MMM YYYY') {
+                return (value == null)
+                    ? ''
+                    : moment(value, 'YYYY-MM-DD').format(fmt)
+            },
+            onPaginationData (paginationData) {
+                this.$refs.pagination.setPaginationData(paginationData)
+                this.$refs.paginationInfo.setPaginationData(paginationData)
+            },
+            onChangePage (page) {
+                this.$refs.vuetable.changePage(page)
+            },
+        },
+        events: {
+            'filter-set' (filterText) {
+                this.moreParams = {
+                    filter: filterText
+                }
+                Vue.nextTick( () => this.$refs.vuetable.refresh() )
+            },
+            'reload-table' () {
+                Vue.nextTick( () => this.$refs.vuetable.reload() )
+            },
+            'filter-reset' () {
+                this.moreParams = {}
+                Vue.nextTick( () => this.$refs.vuetable.refresh() )
+            }
+        }
+    }
+</script>
+<style>
+    .pagination {
+        margin: 0;
+        float: right;
+    }
+    .pagination a.page {
+        border: 1px solid lightgray;
+        border-radius: 3px;
+        padding: 5px 10px;
+        margin-right: 2px;
+    }
+    .pagination a.page.active {
+        color: white;
+        background-color: #337ab7;
+        border: 1px solid lightgray;
+        border-radius: 3px;
+        padding: 5px 10px;
+        margin-right: 2px;
+    }
+    .pagination a.btn-nav {
+        border: 1px solid lightgray;
+        border-radius: 3px;
+        padding: 5px 7px;
+        margin-right: 2px;
+    }
+    .pagination a.btn-nav.disabled {
+        color: lightgray;
+        border: 1px solid lightgray;
+        border-radius: 3px;
+        padding: 5px 7px;
+        margin-right: 2px;
+        cursor: not-allowed;
+    }
+    .pagination-info {
+        float: left;
+    }
+</style>
