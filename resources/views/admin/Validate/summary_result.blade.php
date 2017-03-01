177a7ae622c19920542fbc02bc9d9ad6d32bb9e3
laizamusa <chabi050613@gmail.com>
2017-03-01 16:48:33 +0800

    added chart result

--- resources/views/admin/Validate/summary_result.blade.php
+++ resources/views/admin/Validate/summary_result.blade.php
@@ -1,10 +1,31 @@
 @extends('layouts.admin')
 
+@section('c3scripts')
+    <script src="{{ asset('js/d3/d3.min.js') }}"></script>
+    <script src="{{ asset('js/c3js-chart/c3.min.js') }}"></script>
+    <script src="{{ asset('js/charts-c3.js') }}"></script>
+    <script>
+        var chart = c3.generate({
+            bindto: '#chart',
+            data: {
+                columns: [
+                    ['data1', 1030, 1200, 1100, 1400, 1150, 1250],
+                    ['data2', 2130, 2100, 2140, 2200, 2150, 1850]
+                ],
+                type: 'bar',
+            },
+            bar: {
+                zerobased: false
+            }
+        });
+    </script>
 
-
-<link href="{{ asset('css/c3.css') }}" rel="stylesheet" type="text/css">
+@endsection
 
 @section('content')
+
+    <link href="{{ asset('css/c3.css') }}" rel="stylesheet" type="text/css">
+
     <div class="container">
         <div class="row" >
             <div class="col-sm-6">
@@ -17,25 +38,5 @@
 
         <div id="chart"></div>
 
-
     </div>
 @endsection
-
-<script src="{{ asset('js/d3/d3.min.js') }}"></script>
-<script src="{{ asset('js/c3js-chart/c3.min.js') }}"></script>
-<script src="{{ asset('js/charts-c3.js') }}"></script>
-<script>
-    var chart = c3.generate({
-        bindto: '#chart',
-        data: {
-            columns: [
-                ['data1', 1030, 1200, 1100, 1400, 1150, 1250],
-                ['data2', 2130, 2100, 2140, 2200, 2150, 1850]
-            ],
-            type: 'bar',
-        },
-        bar: {
-            zerobased: false
-        }
-    });
-</script>
