7efb8452b8b34958b19f14b328ad4425f762b324
laizamusa <chabi050613@gmail.com>
2017-03-02 17:43:15 +0800

    Added Modal Dashboard and update loading of Jo List

--- app/Http/Controllers/Front/Validate/ValidateController.php
+++ app/Http/Controllers/Front/Validate/ValidateController.php
@@ -5,6 +5,9 @@ namespace App\Http\Controllers\Front\Validate;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Models\JobOrder;
+use App\Models\JobOrderClient;
+use App\Models\UserProfile;
+use App\Models\Client;
 
 class ValidateController extends Controller
 {
@@ -20,10 +23,59 @@ class ValidateController extends Controller
      */
     public function index()
     {
+        $results = array();
+
         $jos = JobOrder::all();
+        foreach ($jos as $jo){
+            $userProfile = UserProfile::where('user_id', $jo->user_id)->first();
+            $joc = JobOrderClient::where('job_order_id', $jo->id)->first();
+
+            $client = Client::where('id', $joc->client_id)->first();
+
+            $strBrands = '';
+            $i = 1;
+            $t = count($joc->brands);
+            foreach ($joc->brands as $brand) {
+                if( $i != $t ){
+                    $strBrands .= $brand->name.', ';
+                }else{
+                    $strBrands .= $brand->name;
+                }
+                $i++;
+            }
+
+            $arrProjectType = array();
+            $arrProjectType = json_decode($jo->project_types);
+//            dd($arrProjectType);
+            $projecttypes = '';
+            $project = 1;
+            $types = count($arrProjectType);
+            foreach ($arrProjectType as $pr) {
+                if( $project != $types ){
+                    $projecttypes .= $pr->name.', ';
+                }else{
+                    $projecttypes .= $pr->name;
+                }
+                $project++;
+            }
+
+            $jobArray = array(
+                'joId' => $jo->job_order_no,
+                'assigned' => $userProfile->last_name.', '.$userProfile->first_name,
+                'projName' => $jo->project_name,
+                'contact' => $client->contact_person,
+                'brands' => $strBrands,
+                'status' => $jo->status,
+                'projecttypes' => $projecttypes
+            );
+            array_push($results, $jobArray);
+        }
+//            dd($results);
+//        $joc = JobOrderClient::all();
 //        dd($jos);
-        return view('admin/validate', compact('jos'));
+        return view('admin/validate', compact('results'));
     }
+
     /**
      * Display a listing of the resource.
      *
--- resources/views/admin/validate.blade.php
+++ resources/views/admin/validate.blade.php
@@ -24,18 +24,19 @@
                         </tr>
                         </thead>
                         <tbody>
-                        @foreach($jos as $jo)
+                        @foreach($results as $jo)
 
                             <tr>
-                                <td><a href="/validate/create_project"> {{$jo->job_order_no}}</a></td>
-                                <td>{{$jo->project_name}}</td>
-                                <td>{{$jo->project_types}}</td>
-                                <td>dolor</td>
-                                <td>sit</td>
-                                <td>{{$jo->status}}</td>
+                                <td><a href="/validate/create_project"> {{ $jo['joId'] }} </a></td>
+                                <td>{{ $jo['projName'] }}</td>
+                                <td>{{$jo['projecttypes']}}</td>
+                                <td>{{ $jo['contact'] }}</td>
+                                <td>{{ $jo['brands'] }}</td>
+                                {{--<td>sit</td>--}}
+                                <td>{{ $jo['status'] }}</td>
                                 <td>
                                     <a href="/validate/summary_result" class="btn btn-success btn-rounded btn-ripple"><i class="fa fa-eye" aria-hidden="true"></i></a>
-                                    <a href="#" class="btn btn-warning btn-rounded btn-ripple editButtonEvent" alt="'.$row->jobID.'" data-toggle="modal" data-target="#myModal"><i class="fa fa-pencil" aria-hidden="true"></i></a>
+                                        <a href="#" class="btn btn-warning btn-rounded btn-ripple editButtonEvent" alt="'.$row->jobID.'" data-toggle="modal" data-target="#myModal"><i class="fa fa-pencil" aria-hidden="true"></i></a>
                                     <a href="#" class="btn btn-danger btn-rounded btn-ripple deleteButtonEvent" alt="'.$row->jobID.'"><i class="fa fa-trash" aria-hidden="true"></i></i></a>
                                 </td>
                             </tr>
@@ -45,5 +46,61 @@
                 </div>
             </main> 
         </div>
+            <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel">
+                <div class="modal-dialog modal-lg" role="document" style="width: 570px;">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                            <h4 class="modal-title" id="myModalLabel">Edit Dashboard</h4>
+                        </div>
+                        <div class="modal-body">
+                            <form id="clientForm">
+                                <div class="row">
+                                    <div class="col-md-offset-1 col-md-10 form-group text-input-container">
+                                        <label class="control-label">JO Order Number</label>
+                                        <input type="text" name="JO_Order_Number" placeholder="JO Order Number" class="form-control" />
+                                        {{--<v-select :on-change="roleSelected" :options="roleOptions"></v-select>--}}
+                                    </div>
+
+                                    <div class="col-md-offset-1 col-md-10 form-group text-input-container">
+                                        <label class="control-label">Project Name</label>
+                                        <input type="text" name="Project_Name" placeholder="Project Name" class="form-control" />
+                                        {{--<v-select :on-change="departmentSelected" :options="departmentOptions"></v-select>--}}
+                                    </div>
+
+                                    <div class="col-md-offset-1 col-md-10 form-group text-input-container">
+                                        <label class="control-label">Project Type</label>
+                                        <input type="text" name="ptype"
+                                        placeholder="Project Type" class="form-control" />
+                                    </div>
+
+                                    <div class="col-md-offset-1 col-md-10 form-group text-input-container">
+                                        <label class="control-label" >Client Name</label>
+                                        <input type="text" name="Client_name" placeholder="Client Name" class="form-control" />
+                                        {{--<v-select :on-change="genderSelected" :options="genderOptions"></v-select>--}}
+                                    </div>
+
+                                    <div class="col-md-offset-1 col-md-10 form-group text-input-container">
+                                        <label class="control-label">Brand</label>
+                                        <input type="text" name="brands"
+                                        placeholder="Brand" class="form-control" />
+                                    </div>
+
+                                    <div class="col-md-offset-1 col-md-10 form-group text-input-container">
+                                        <label class="control-label">Status</label>
+                                        <input type="text" name="Status"
+                                        {{--@input="inputChange" v-bind:value="Status" id="Status"--}}
+                                        placeholder="Status" class="form-control" />
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                            <button type="button" class="btn btn-primary" @click="saveProject">Save</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
     </div>
 @endsection
