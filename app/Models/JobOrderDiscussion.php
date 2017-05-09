9bf9b29682864fa45d89582567c78d79325a62f5
Alleo Indong <freedom_war550@live.com.ph>
2017-05-09 11:54:00 +0800

    updated job order discussion, added user relation

--- app/Models/JobOrderDiscussion.php
+++ app/Models/JobOrderDiscussion.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\User;
 use Illuminate\Database\Eloquent\Model;
 
 class JobOrderDiscussion extends Model
@@ -15,5 +16,10 @@ class JobOrderDiscussion extends Model
     public function jobOrder()
     {
         return $this->belongsTo(JobOrder::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
     }
 }
