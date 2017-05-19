cf7e0904ce7c04c35812d2e0d205ec1106eab632
Alleo Indong <freedom_war550@live.com.ph>
2017-05-19 13:16:01 +0800

    updated form on job order

--- resources/assets/js/components/job-orders/inventory/commons/form.vue
+++ resources/assets/js/components/job-orders/inventory/commons/form.vue
@@ -19,6 +19,15 @@
             </div>
         </div>
 
+        <div class="col-md-6 form-group text-input-container">
+            <label class="control-label col-sm-12" for="expected_quantity">Expected Delivery Date</label>
+            <div class="col-md-12">
+                <input type="date" name="expected_delivery_date" required placeholder="Expected Delivery Date"
+                       id="expected_delivery_date" class="form-control"
+                       @input="inputChange" v-bind:value="expected_delivery_date"/>
+            </div>
+        </div>
+
         <div class="col-md-12 text-right form-group select-input-container">
             <button type="submit" style="width: 200px" class="btn btn-primary btn-add" @click="saveProject">Save</button>
         </div>
@@ -35,7 +44,8 @@
             return {
                 item_name: '',
                 expected_quantity: '',
-                job_order_id: ''
+                job_order_id: '',
+                expected_delivery_date: ''
             }
         },
         mounted() {
@@ -44,6 +54,7 @@
             resetForm() {
                 this.item_name = ''
                 this.expected_quantity = ''
+                this.expected_delivery_date = ''
             },
             inputChange(e) {
                 this[e.target.id] = e.target.value
@@ -53,7 +64,8 @@
                 let data = {
                     job_order_id: jobOrderId,
                     expected_quantity: this.expected_quantity,
-                    item_name: this.item_name
+                    item_name: this.item_name,
+                    expected_delivery_date: this.expected_delivery_date
                 }
 
                 let url = `/api/v1/job-order-inventory`;
