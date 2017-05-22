0dfae989e55c51291333406417f307a19a02b1d6
Jose Emmanuel Manangan <noel1231@users.noreply.github.com>
2017-05-22 18:47:01 +0800

    Inventory - add AE handler in prints

--- resources/views/inventory/print/header.blade.php
+++ resources/views/inventory/print/header.blade.php
@@ -2,10 +2,13 @@
 <div class="row" style="padding-top: 50px">
     <div class="col-md-6 col-sm-6 col-xs-6">
         <h5>
-            <b>JOB ORDER NO.:</b> {{ $iJob->jobOrder->job_order_no }}
+            <b>JOB ORDER NUMBER:</b> {{ $iJob->jobOrder->job_order_no }}
         </h5>
         <h5>
-            <b>PROJECT NAME.:</b> {{ $iJob->jobOrder->project_name }}
+            <b>PROJECT NAME:</b> {{ $iJob->jobOrder->project_name }}
+        </h5>
+        <h5>
+            <b>AE:</b> {{ $iJob->jobOrder->user->profile->first_name }} {{ $iJob->jobOrder->user->profile->last_name }}
         </h5>
     </div>
     <div class="col-md-6 col-sm-6 col-xs-6 text-right">
