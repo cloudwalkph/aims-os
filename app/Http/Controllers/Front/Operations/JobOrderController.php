56b57a592e22b8bb35acd6110f4d515c4936bc90
Nina Kristel Cheng <nkmcheng@live.com>
2017-06-08 14:36:09 +0800

    added controllers

---
+++ app/Http/Controllers/Front/Operations/JobOrderController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Controllers\Front\Operations;
+
+use App\Http\Controllers\Controller;
+use App\Models\Department;
+use App\Models\JobOrder;
+use App\Models\JobOrderAddUser;
+use App\Models\JobOrderAnimationDetail;
+use App\Models\JobOrderDepartmentInvolved;
+use App\Models\JobOrderDetail;
+use App\Models\JobOrderProduct;
+use App\Models\JobOrderManpower;
+use App\Models\JobOrderMeal;
+use App\Models\JobOrderMom;
+use App\Models\JobOrderProjectAttachment;
+use App\Models\JobOrderVehicle;
+use App\Models\ManpowerType;
+use App\Models\MealType;
+use App\Models\VehicleType;
+use Illuminate\Http\Request;
+
+class JobOrderController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        config(['app.name' => 'Operations | AIMS']);
+
+        return view('operations.joborder.index');
+    }
+}
