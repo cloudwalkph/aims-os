df757498c29c353c9fe383136a092386a5633dc2
Alleo Indong <freedom_war550@live.com.ph>
2017-06-15 14:37:00 +0800

    updated bootstrap

--- resources/assets/js/bootstrap.js
+++ resources/assets/js/bootstrap.js
@@ -112,8 +112,13 @@ window.axios.defaults.headers.common = {
 
 import Echo from "laravel-echo"
 
-window.Echo = new Echo({
-    broadcaster: 'socket.io',
-    host: 'localhost:6001'
-    // key: 'your-pusher-key'
-});
+try {
+    window.Echo = new Echo({
+        broadcaster: 'socket.io',
+        host: 'localhost:6001'
+        // key: 'your-pusher-key'
+    });
+} catch (err) {
+    console.log('no io');
+}
+
