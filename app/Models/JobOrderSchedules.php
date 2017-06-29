5d2f0bd4372dc932bbb366096a8432c5ea5f1de6
Alleo Indong <freedom_war550@live.com.ph>
2017-06-29 11:17:42 +0800

    added job order schedules

--- app/Models/JobOrderSchedules.php
+++ app/Models/JobOrderSchedules.php
@@ -3,14 +3,22 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class JobOrderSchedules extends Model
 {
+    use SoftDeletes;
+
     protected $table = 'job_order_schedules';
-    protected $fillable = ['job_order_id', 'description', 'venue_id', 'jo_datetime', 'status'];
+    protected $fillable = ['job_order_id', 'remarks', 'venue_id', 'jo_datetime', 'status'];
 
     public function jobOrder()
     {
         return $this->belongsTo(JobOrder::class);
+    }
+
+    public function venue()
+    {
+        return $this->belongsTo(Venue::class);
     }
 }
