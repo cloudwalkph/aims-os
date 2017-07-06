49e52fff1097e49cd390dc2c4ca538a224635752
Alleo Indong <freedom_war550@live.com.ph>
2017-07-06 15:27:02 +0800

    updated discussion

--- resources/views/components/jo/content/discussion.blade.php
+++ resources/views/components/jo/content/discussion.blade.php
@@ -2,12 +2,12 @@
 
 @section('jo-details-content')
     <div class="tab-pane" id="discussions">
-        <form method="POST" class="form-horizontal" style="margin-top: 20px">
+        {{--<form method="POST" class="form-horizontal" style="margin-top: 20px">--}}
             <div class="box box-info">
                 <div class="box-body" style="padding: 10px 30px">
                     <discussions></discussions>
                 </div>
             </div>
-        </form>
+        {{--</form>--}}
     </div>
 @endsection
